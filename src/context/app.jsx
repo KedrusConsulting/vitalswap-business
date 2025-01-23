0f099502857929db6dd80d84b107dce6bb3a4f2f
iamwhitegod <kmbagwu12@gmail.com>
2025-01-23 12:33:36 +0100

    chore: removed on used code

--- src/context/app.jsx
+++ src/context/app.jsx
@@ -1,34 +1,9 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import axios from "axios";
+import { createContext, useContext } from "react";
 
 const AppContext = createContext();
 export const useApp = () => useContext(AppContext);
 
 const AppProvider = ({ children }) => {
-  const [rate, setRate] = useState({});
-  const [faqs, setFaqs] = useState();
-  const [featuredBlogs, setFeaturedBlogs] = useState();
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const { data } = await axios.get(
-          "https://vitalswap.com/test/api_v2/utils/webHome"
-        );
-
-        const { rate, featuredBlogPosts, faq } = data;
-
-        setRate(rate);
-        setFaqs(faq);
-        setFeaturedBlogs(featuredBlogPosts);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getData();
-  }, []);
-
   return (
     <AppContext.Provider value={{ rate, faqs, featuredBlogs }}>
       {children}
