132a8c264bc588f9d123d68ea45f6122418ad94e
iamwhitegod <kmbagwu12@gmail.com>
2025-01-23 12:48:46 +0100

    fix: reverted changes

--- src/context/app.jsx
+++ src/context/app.jsx
@@ -1,9 +1,34 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
 
 const AppContext = createContext();
 export const useApp = () => useContext(AppContext);
 
 const AppProvider = ({ children }) => {
+  const [rate, setRate] = useState({});
+  const [faqs, setFaqs] = useState();
+  const [featuredBlogs, setFeaturedBlogs] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://vitalswap.com/test/api_v2/utils/webHome"
+        );
+
+        const { rate, featuredBlogPosts, faq } = data;
+
+        setRate(rate);
+        setFaqs(faq);
+        setFeaturedBlogs(featuredBlogPosts);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
     <AppContext.Provider value={{ rate, faqs, featuredBlogs }}>
       {children}
